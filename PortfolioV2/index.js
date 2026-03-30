00e1395827f17794751821a49be0d8fe1dd07820
Andrew Scott Pitblado <apitblado@gmail.com>
2026-03-30 11:06:09 -0400

    Update jounrey timeline: removed hover animation to make it more mobile friendly, added lick to expand chevron icons, removed a lot of JS

--- PortfolioV2/index.js
+++ PortfolioV2/index.js
@@ -1,4 +1,3 @@
-const timelineShell = document.querySelector(".timeline-shell");
 const lightIcon = "assets/cool-light.svg";
 const darkIcon = "assets/cool-dark.svg";
 
@@ -15,34 +14,3 @@ const updateFavicon = (isDark) => {
 const media = window.matchMedia("(prefers-color-scheme: dark)");
 updateFavicon(media.matches);
 media.addEventListener("change", (event) => updateFavicon(event.matches));
-
-if (timelineShell) {
-  const timelineCards = timelineShell.querySelectorAll(".timeline-card");
-
-  const resetCardScroll = (card) => {
-    // Wait until hover/focus styles are removed, then reset internal scroll.
-    requestAnimationFrame(() => {
-      if (!card.matches(":hover") && !card.matches(":focus-within")) {
-        card.scrollTop = 0;
-      }
-    });
-  };
-
-  timelineCards.forEach((card) => {
-    card.addEventListener("mouseleave", () => {
-      resetCardScroll(card);
-    });
-
-    card.addEventListener("focusout", () => {
-      resetCardScroll(card);
-    });
-
-    card.addEventListener(
-      "touchend",
-      () => {
-        resetCardScroll(card);
-      },
-      { passive: true },
-    );
-  });
-}
