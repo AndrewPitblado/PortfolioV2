84983a20581d04b5e4114e06badee283d497a46f
Andrew Scott Pitblado <apitblado@gmail.com>
2026-03-30 01:08:05 -0400

    Added JS to auto switch favicon depending on system settings

--- PortfolioV2/index.js
+++ PortfolioV2/index.js
@@ -1,4 +1,20 @@
 const timelineShell = document.querySelector(".timeline-shell");
+const lightIcon = "assets/cool-light.svg";
+const darkIcon = "assets/cool-dark.svg";
+
+const updateFavicon = (isDark) => {
+  let favicon = document.querySelector('link[rel="icon"]:not([media])');
+  if (!favicon) {
+    favicon = document.createElement("link");
+    favicon.rel = "icon";
+    document.head.appendChild(favicon);
+  }
+  favicon.href = isDark ? darkIcon : lightIcon;
+};
+
+const media = window.matchMedia("(prefers-color-scheme: dark)");
+updateFavicon(media.matches);
+media.addEventListener("change", (event) => updateFavicon(event.matches));
 
 if (timelineShell) {
   const timelineCards = timelineShell.querySelectorAll(".timeline-card");
